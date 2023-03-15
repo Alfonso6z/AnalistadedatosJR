d18be2717a9f3e8af223970063e107ec53980052
Alfonso6z <alfonso6z.w@gmail.com>
2023-03-14 20:09:45 -0600

    sieteymedio

--- sieteymedio/jugadorSiete.py
+++ sieteymedio/jugadorSiete.py
@@ -1,4 +1,42 @@
 from jugador import Jugador
+from carta import Carta
 class JugadorSiete(Jugador):
-    def __init__(self, nick, humano):
-        pass
+    
+    def __init__(self, nick, humano,recursos:dict):
+        super().__init__(nick, humano,recursos)
+        self.__mano:list = recursos["mano"]
+        self.__puntos:float = recursos["puntos"]
+    
+    def agregar_carta(self,carta:Carta):
+        self.__mano.append(carta)
+    
+    def contar_puntos(self):
+        self.__puntos = 0
+        for carta in self.__mano:
+            self.__puntos+=carta.valor
+        return self.puntos
+    
+    def mostrar_mano(self):
+        m_cartas:str = ""
+        for carta in self.__mano:
+            m_cartas+=f"({carta.figura},{carta.valor})\n"
+        return m_cartas
+    
+    def __str__(self):
+        return f"""nickname: {self.nick}
+humano:{self.humano}
+puntos:{self.puntos}"""
+    
+    @property
+    def mano(self):
+        return self.__mano
+    @mano.setter
+    def mano(self,mano):
+        self.__mano = mano
+        
+    @property
+    def puntos(self):
+        return self.__puntos
+    @puntos.setter
+    def puntos(self,puntos):
+        self.__puntos = puntos
--- sieteymedio/main.py
+++ sieteymedio/main.py
@@ -1,13 +1,17 @@
 import cartas
+from jugadorSiete import JugadorSiete
 
 def main():
+    j1 = JugadorSiete("El albert", True, {"mano":[],"puntos":0.0})
     baraja = cartas.crear_cartas()
-    print(baraja[0])
-    print(type(baraja[0]))
-    print(baraja[0].valor)
-    print(type(baraja[0].valor))
-    print(baraja[0].figura)
-    print(type(baraja[0].figura))
+    print(j1)
+    j1.agregar_carta(baraja.pop())
+    j1.agregar_carta(baraja.pop())
+    print(j1.mostrar_mano())
+    print(j1.contar_puntos())
+    print(j1.contar_puntos())
+
+    
     
 if __name__ == "__main__":
     main()
